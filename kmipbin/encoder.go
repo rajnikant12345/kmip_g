a98a33897eabc12b072a583b6d77550b6b456962
rajnikant <rajnikant12345@gmail.com>
2018-12-28 21:27:42 +0530

    adding ttlv reader commit

--- kmipbin/encoder.go
+++ kmipbin/encoder.go
@@ -6,6 +6,28 @@ import (
 	"math/big"
 )
 
+
+
+type KmipLength uint32
+
+func (k *KmipLength) MarshalBin() []byte {
+	i := *k
+	b := make([]byte,4)
+	binary.BigEndian.PutUint32(b[:4],uint32(i))
+	return b
+}
+
+
+func (k *KmipLength) UnMarshalBin( b []byte  ) error {
+	if len(b) != 4 {
+		return errors.New("Invalid KmipInt")
+	}
+	*k = KmipLength( binary.BigEndian.Uint32(b) )
+	return nil
+}
+
+
+
 type KmipInt uint32
 
 func (k *KmipInt) MarshalBin() []byte {
@@ -24,6 +46,48 @@ func (k *KmipInt) UnMarshalBin( b []byte  ) error {
 	return nil
 }
 
+
+type KmipEnum uint32
+
+func (k *KmipEnum) MarshalBin() []byte {
+	i := *k
+	b := make([]byte,8)
+	binary.BigEndian.PutUint32(b[:4],uint32(i))
+	return b
+}
+
+
+func (k *KmipEnum) UnMarshalBin( b []byte  ) error {
+	if len(b) != 8 {
+		return errors.New("Invalid KmipInt")
+	}
+	*k = KmipEnum( binary.BigEndian.Uint32(b[:4]) )
+	return nil
+}
+
+
+
+type KmipInterval uint32
+
+func (k *KmipInterval) MarshalBin() []byte {
+	i := *k
+	b := make([]byte,8)
+	binary.BigEndian.PutUint32(b[:4],uint32(i))
+	return b
+}
+
+
+func (k *KmipInterval) UnMarshalBin( b []byte  ) error {
+	if len(b) != 8 {
+		return errors.New("Invalid KmipInt")
+	}
+	*k = KmipInterval( binary.BigEndian.Uint32(b[:4]) )
+	return nil
+}
+
+
+
+
 type KmipLongInt uint64
 
 
@@ -40,6 +104,46 @@ func (k *KmipLongInt) UnMarshalBin( b []byte  ) error {
 		return errors.New("Invalid KmipInt")
 	}
 	*k = KmipLongInt( binary.BigEndian.Uint64(b) )
+	return nil
+}
+
+
+type KmipBoolean uint64
+
+
+func (k *KmipBoolean) MarshalBin() []byte {
+	i := *k
+	b := make([]byte,8)
+	binary.BigEndian.PutUint64(b,uint64(i))
+	return b
+}
+
+
+func (k *KmipBoolean) UnMarshalBin( b []byte  ) error {
+	if len(b) != 8 {
+		return errors.New("Invalid KmipInt")
+	}
+	*k = KmipBoolean( binary.BigEndian.Uint64(b) )
+	return nil
+}
+
+
+type KmipDate uint64
+
+
+func (k *KmipDate) MarshalBin() []byte {
+	i := *k
+	b := make([]byte,8)
+	binary.BigEndian.PutUint64(b,uint64(i))
+	return b
+}
+
+
+func (k *KmipDate) UnMarshalBin( b []byte  ) error {
+	if len(b) != 8 {
+		return errors.New("Invalid KmipInt")
+	}
+	*k = KmipDate( binary.BigEndian.Uint64(b) )
 	return nil
 }
 
@@ -108,3 +212,11 @@ func (k *KmipBigInt) UnMarshalBin( b []byte , length int  ) error {
 
 	return nil
 }
+
+
+type KmipStruct interface {
+	MarsalBin() []byte
+	UnMarshalBin() error
+}
+
+
---
+++ kmipbin/ttlv.go
@@ -0,0 +1,63 @@
+package kmipbin
+
+import (
+	"bytes"
+	"io"
+)
+
+
+func TTLVReader(reader io.ReadWriter) ([]byte, error) {
+	out  :=  bytes.Buffer{}
+
+	initialBytes := make([]byte,8)
+
+	tmp := initialBytes
+
+	var expected KmipLength
+	// we expect 8 bytes initially
+	expected = 8
+	for {
+		// read first 8 bytes
+		b,err := reader.Read(tmp)
+		if err != nil {
+			return nil, err
+		}
+		// if 8 bytes are not recieved, just try tp read more
+		if b < int(expected) {
+			tmp = tmp[b:]
+		}else {
+			break
+		}
+		expected = expected - KmipLength(b)
+	}
+
+	out.Write(initialBytes)
+	// read expected bytes from KMIP buffer
+	expected.UnMarshalBin(initialBytes[4:])
+
+	// create a buffer to contain full kmip bytes
+	fullBytes := make([]byte,expected)
+
+	// use temp buffer to hold partial data
+	tmp = fullBytes
+
+
+	for {
+		b,err := reader.Read(tmp)
+		if err != nil {
+			return nil, err
+		}
+		if b < int(expected) {
+			tmp = tmp[b:]
+		}else {
+			break
+		}
+		expected = expected - KmipLength(b)
+	}
+	out.Write(fullBytes)
+
+	return out.Bytes(), nil
+}
+
+
+
